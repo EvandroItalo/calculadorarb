654e6329a74f2a9b0c35e3cb60671da19df2d22a
evandroitalo <evandro.italo@gmail.com>
2024-10-23 00:09:19 -0300

    Desafio calculadora DIO feita em ruby finalizado

--- calculadora.rb
+++ calculadora.rb
@@ -1,1 +1,50 @@
-puts "***** Calculadora Braba *****"
+loop do
+    puts "-----------------------------"
+    puts "***** Calculadora Braba *****"
+    puts "-----------------------------"
+    puts "*      Digite uma opção     *"
+    puts "-----------------------------"
+    puts "* 1 - Soma           ( + )  *"
+    puts "* 2 - Subtração      ( - )  *"
+    puts "* 3 - Multiplicação  ( * )  *"
+    puts "* 4 - Divisão        ( / )  *"
+    puts "* 0 - Sair                  *"
+    puts "-----------------------------"
+    print "Digite sua opção: "
+    opcao = gets.chomp.to_i
+
+    case opcao
+        when opcao = 1
+            print "Digite o primeiro numero: "
+            num1 = gets.chomp.to_i
+            puts "Digite o segundo numero: "
+            num2 = gets.chomp.to_i
+            resultado = num1 + num2
+            puts "#{num1} + #{num2} = #{resultado}"
+        when opcao = 2
+            print "Digite o primeiro numero: "
+            num1 = gets.chomp.to_i
+            print "Digite o segundo numero: "
+            num2 = gets.chomp.to_i
+            resultado = num1 - num2
+            puts "#{num1} - #{num2} = #{resultado}"
+        when opcao = 3
+            print "Digite o primeiro numero: "
+            num1 = gets.chomp.to_i
+            print "Digite o segundo numero: "
+            num2 = gets.chomp.to_i
+            resultado = num1 * num2
+            puts "#{num1} * #{num2} = #{resultado}"
+        when opcao = 4
+            print "Digite o primeiro numero: "
+            num1 = gets.chomp.to_i
+            print "Digite o segundo numero: "
+            num2 = gets.chomp.to_i
+            resultado = num1 / num2
+            puts "#{num1} / #{num2} = #{resultado}"
+        when opcao = 0
+            break 
+    end
+
+    system "clear"
+end
